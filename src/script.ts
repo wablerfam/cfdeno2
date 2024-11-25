7bb86cf9f4403720d3137ede535d3cfe474d8798
wablerfam <wablerfam@gmail.com>
2024-11-25 20:35:54 +0900

    feat: remove try and catch

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,7 @@
 import { type Context, register } from "@kt3k/cell";
 import * as SimpleWebAuthnBrowser from "@simplewebauthn/browser";
 import { hc } from "hono/client";
+import { ResultAsync } from "neverthrow";
 
 import { AuthAppType } from "./server.tsx";
 
@@ -8,80 +9,78 @@ const authClient = hc<AuthAppType>("/");
 
 const Auth = ({ on, query }: Context) => {
   on("click", ".register", async () => {
-    try {
-      const userName = query<HTMLInputElement>(".userName")?.value;
-      if (!userName) {
-        alert("名前を入れてください");
-        return;
-      }
-
-      const optionsResponse = await authClient.auth.attestation.option.$get(
-        { query: { userName: userName } },
-      );
-      const { options } = await optionsResponse.json();
-
-      const registration = await SimpleWebAuthnBrowser.startRegistration({
-        optionsJSON: options,
-      });
-
-      const verificationResponse = await authClient.auth.attestation.result.$post(
-        {
-          json: { userName, body: registration },
-        },
-        {
-          headers: { "Content-Type": "application/json" },
-        },
-      );
-
-      const verification = await verificationResponse.json();
-      if (verification.verified) {
-        alert("登録に成功しました");
-      } else {
-        // alert(`登録に失敗しました: ${verification.error}`);
-        alert(`登録に失敗しました:`);
-      }
-    } catch (e) {
-      alert(`登録に失敗しました: ${e}`);
+    const userName = query<HTMLInputElement>(".userName")?.value;
+    if (!userName) {
+      alert("名前を入れてください");
+      return;
     }
+
+    const optionsResponse = await authClient.auth.attestation.option.$get(
+      { query: { userName: userName } },
+    );
+    const { options } = await optionsResponse.json();
+
+    const startRegistrationAsync = ResultAsync.fromThrowable(SimpleWebAuthnBrowser.startRegistration);
+    const registration = await startRegistrationAsync({ optionsJSON: options });
+    if (!registration.isOk()) {
+      alert(`認証に失敗しました: ${registration.error}`);
+      return;
+    }
+
+    const verificationResponse = await authClient.auth.attestation.result.$post(
+      {
+        json: { userName, body: registration.value },
+      },
+      {
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+
+    const verification = await verificationResponse.json();
+    if (!verification.verified) {
+      alert("登録に失敗しました");
+      return;
+    }
+
+    alert("登録に成功しました");
   });
 
   on("click", ".verify", async () => {
-    try {
-      const userName = query<HTMLInputElement>(".userName")?.value;
-      if (!userName) {
-        alert("名前を入れてください");
-        return;
-      }
-
-      const optionsResponse = await authClient.auth.assertion.option.$get(
-        { query: { userName: userName } },
-      );
-      const { options } = await optionsResponse.json();
-
-      const authentication = await SimpleWebAuthnBrowser.startAuthentication({
-        optionsJSON: options,
-      });
-
-      const verificationResponse = await authClient.auth.assertion.result.$post(
-        {
-          json: { userName, body: authentication },
-        },
-        {
-          headers: { "Content-Type": "application/json" },
-        },
-      );
-
-      const verification = await verificationResponse.json();
-
-      if (verification.verified) {
-        alert("認証に成功しました");
-      } else {
-        // alert(`認証に失敗しました: ${verification.error}`);
-        alert(`登録に失敗しました:`);
-      }
-    } catch (e) {
-      alert(`認証に失敗しました: ${e}`);
+    const userName = query<HTMLInputElement>(".userName")?.value;
+    if (!userName) {
+      alert("名前を入れてください");
+      return;
     }
+
+    const optionsResponse = await authClient.auth.assertion.option.$get(
+      { query: { userName: userName } },
+    );
+    const { options } = await optionsResponse.json();
+
+    const startAuthenticationAsync = ResultAsync.fromThrowable(SimpleWebAuthnBrowser.startAuthentication);
+    const authentication = await startAuthenticationAsync({ optionsJSON: options });
+    if (!authentication.isOk()) {
+      alert(`認証に失敗しました: ${authentication.error}`);
+      return;
+    }
+
+    const verificationResponse = await authClient.auth.assertion.result.$post(
+      {
+        json: { userName, body: authentication.value },
+      },
+      {
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+
+    const verification = await verificationResponse.json();
+
+    if (!verification.verified) {
+      alert("登録に失敗しました");
+      return;
+    }
+
+    alert("登録に成功しました");
   });
 };
 
